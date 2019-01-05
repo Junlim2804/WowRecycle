bcbbe93d466f27d87d8562ea5c468cb4bd770214
Junlim2804 <limtj-pa16@student.tarc.edu.my>
2019-01-05 12:53:30 +0800

    locationpicker

--- app/src/main/java/com/example/user/wowrecycle/BookingFragment.java
+++ app/src/main/java/com/example/user/wowrecycle/BookingFragment.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,17 +15,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.util.Calendar;
 
+import static android.app.Activity.RESULT_OK;
+
 
 public class BookingFragment extends DialogFragment {
-
+    private Button btnsetLocation;
     private static final String TAG = "BookingFragment";
     private TextView dateData;
     private DatePickerDialog.OnDateSetListener mDataSetListener;
+    int PLACE_PICKER_REQUEST =1;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data,getActivity());
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 
 
     @Nullable
@@ -41,6 +62,23 @@ public class BookingFragment extends DialogFragment {
                 DatePickerDialog dialog=new DatePickerDialog(getActivity(),mDataSetListener,year,month,day);
                 dialog.show();
             }
+        });
+        btnsetLocation=(Button)v.findViewById((R.id.btnLocation));
+        btnsetLocation.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                PlacePicker.IntentBuilder builder=new PlacePicker.IntentBuilder();
+                Intent intent = null;
+                try {
+                    intent = builder.build(getActivity());
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+                // Start the Intent by requesting a result, identified by a request code.
+                startActivityForResult(intent, PLACE_PICKER_REQUEST);
+            }
+
         });
         mDataSetListener=new DatePickerDialog.OnDateSetListener(){
             @Override
