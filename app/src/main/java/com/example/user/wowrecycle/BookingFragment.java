304d2cd910606e8fc1b21f93b8303756ebf6a09e
Junlim2804 <limtj-pa16@student.tarc.edu.my>
2019-01-06 00:28:36 +0800

    booking add camera image booking layout change to table

--- app/src/main/java/com/example/user/wowrecycle/BookingFragment.java
+++ app/src/main/java/com/example/user/wowrecycle/BookingFragment.java
@@ -1,22 +1,32 @@
 package com.example.user.wowrecycle;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +35,7 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Calendar;
 
 import static android.app.Activity.RESULT_OK;
@@ -32,10 +43,19 @@ import static android.app.Activity.RESULT_OK;
 
 public class BookingFragment extends DialogFragment {
     private Button btnsetLocation;
+
+    private static int RESULT_LOAD_IMG = 4;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL = 2;
+    private static int REQUEST_IMAGE_CAPTURE=3;
+    private ImageView uploader;
+    Bitmap bitmap;
+    String imageString;
+
     private static final String TAG = "BookingFragment";
     private TextView dateData;
     private DatePickerDialog.OnDateSetListener mDataSetListener;
     int PLACE_PICKER_REQUEST =1;
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
@@ -44,6 +64,22 @@ public class BookingFragment extends DialogFragment {
                 Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
             }
         }
+        else if(requestCode==REQUEST_IMAGE_CAPTURE)
+        {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                bitmap = (Bitmap) extras.get("data");
+                uploader.setImageBitmap(bitmap);
+
+                //converting image to base64 string
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] imageBytes = baos.toByteArray();
+                imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+            }
+        }
+
     }
 
 
@@ -52,6 +88,29 @@ public class BookingFragment extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_booking,container,false);
+
+        if (ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this.getActivity(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
         dateData=(TextView)v.findViewById(R.id.datedata);
         dateData.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
@@ -90,6 +149,20 @@ public class BookingFragment extends DialogFragment {
             }
 
         };
+
+        uploader=(ImageView)v.findViewById(R.id.item_image);
+        uploader.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                }
+
+            }
+        });
+
         return v;
     }
 
