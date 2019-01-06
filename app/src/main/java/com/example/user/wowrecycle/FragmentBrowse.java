4891397eca642535d6cd89870a7f444ded4f6839
keatt <bk0164414797>
2019-01-07 02:09:36 +0800

    points

--- app/src/main/java/com/example/user/wowrecycle/FragmentBrowse.java
+++ app/src/main/java/com/example/user/wowrecycle/FragmentBrowse.java
@@ -1,5 +1,7 @@
 package com.example.user.wowrecycle;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,15 +12,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FragmentBrowse extends Fragment {
     View view;
     private List<Reward> listReward;
     private RecyclerView myRecyclerView;
-
+    private static int currentPoint=0;
+    ProgressDialog progressDialog;
 
 
     public FragmentBrowse(){
@@ -26,8 +42,9 @@ public class FragmentBrowse extends Fragment {
     }
 
     @Nullable
-    @Override
 
+    private TextView textView;
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.browse_fragment, container,false);
         myRecyclerView = (RecyclerView)view.findViewById(R.id.reward_rv);
@@ -35,11 +52,63 @@ public class FragmentBrowse extends Fragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         myRecyclerView.setLayoutManager(linearLayoutManager);
 
+        textView=(TextView)view.findViewById(R.id.tmp_point);
+        getBonus(getActivity(),AppConfig.URL_GETPOINTS);
+        textView.setText(currentPoint+"");
         myRecyclerView.setAdapter(recyclerAdapter);
 
         return view;
     }
 
+    private void getBonus(Context context, String url)
+    {
+        progressDialog = ProgressDialog.show(getActivity(), "Loading...", "Please wait...", true);
+        SQLiteHandler db = new SQLiteHandler(getActivity());
+        HashMap<String, String> user=db.getUserDetails();
+        //String uname=user.get("name");
+        String uname="BK";
+        RequestQueue queue = Volley.newRequestQueue(context);
+        url=url+"?name="+uname;
+         if (!progressDialog.isShowing())
+             progressDialog.setMessage("Syn with server...");
+          progressDialog.show();
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+
+                        try{
+                            //Clear list
+
+
+                           if(response.length()>0){
+                                JSONObject res = (JSONObject) response.get(0);
+                                currentPoint = res.getInt("points");
+                                Toast.makeText(getActivity(), currentPoint+"", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                               if (progressDialog.isShowing())
+                                    progressDialog.dismiss();
+
+                        }catch (Exception e){
+                            Toast.makeText(getActivity(), "Error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(getActivity(), "Error:" + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                         if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
