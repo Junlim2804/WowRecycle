b044a5377f8b17cc4ce3d407811dac0931f27001
keatt <bk0164414797>
2019-08-05 23:16:45 +0800

    final

--- app/src/main/java/com/example/user/wowrecycle/ConfirmActivity.java
+++ app/src/main/java/com/example/user/wowrecycle/ConfirmActivity.java
@@ -2,14 +2,16 @@ package com.example.user.wowrecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class ConfirmActivity extends AppCompatActivity {
 
     private TextView pointsReceived, colAddress, colDate, colTime;
-    private Button btnBack, btnOkay;
+    private Button btnBack, btnOkay, btnok;
 
 
     @Override
@@ -21,8 +23,26 @@ public class ConfirmActivity extends AppCompatActivity {
         colAddress = (TextView) findViewById(R.id.rColAdd);
         colDate = (TextView) findViewById(R.id.rColDate);
         colTime = (TextView) findViewById(R.id.rColTime);
+        btnok = (Button) findViewById(R.id.btnOkay);
+
+
+
+
+        // Capture button clicks
+        btnok.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(ConfirmActivity.this,
+                        MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
 
 
 
     }
-}
+
+
